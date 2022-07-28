47e0b460d06d8b260c5538f37cb6295db535a7ea
Ashwin Sasikumar <ashwin.ashwin366@gmail.com>
2022-07-28 11:42:48 +0530

    made changes to color controller and model

--- vetport_api/models/Color.js
+++ vetport_api/models/Color.js
@@ -2,20 +2,25 @@ const mongoose = require("mongoose");
 const Species = require("./Species");
 const Schema = mongoose.Schema;
 
-const color = new Schema({
-  species_id: {
-    type: Schema.Types.ObjectId,
-    ref: Species.modelName,
+const color = new Schema(
+  {
+    species_id: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Species.modelName,
+      },
+    ],
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: Boolean,
+    },
   },
-  color: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  status: {
-    type: Boolean,
-  },
-});
+  { timestamps: true }
+);
 
 const ColorModel = mongoose.model("color", color);
 module.exports = ColorModel;
