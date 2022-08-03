8f7641dd8edfe036111237d6f1b01584b7b156d4
Ashwin Sasikumar <ashwin.ashwin366@gmail.com>
2022-08-03 19:28:08 +0530

    added api for filter appointment by staff and update appointment, added status field in model

--- vetport_api/routes/v1/public/Appointment.js
+++ vetport_api/routes/v1/public/Appointment.js
@@ -6,6 +6,9 @@ const appointment = require("../../../controllers/Appointment");
 router.post("/save_appointment", appointment.create);
 router.get("/get_appointment", appointment.findAll);
 router.get("/get_appointmentByDate/:from/:to", appointment.findByDate);
+router.get("/get_appointmentByStaff/:date/:staff?", appointment.findByStaff);
+router.put("/update_appointment/:id", appointment.update);
+
 // router.get("/get_appointmentByWeek/:from/:to", appointment.findByWeek);
 // router.get("/get_appointmentByMonth/:year/:month", appointment.findByMonth);
 
